993e0011c7c9bc6e3cbaa5b1c548d302b2a3b939
curtisverbraak <0896588@hr.nl>
2015-10-01 19:55:20 +0200

    new update

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -27,28 +27,56 @@ if computer == "rock" and user == "lizard":
 	sys.stdout.write('the computer won! Rock smashes Lizard!\n')
 elif computer == "rock" and user == "scissors":
 	sys.stdout.write('the computer won! Rock crushes scissors\n')
+
+elif computer == "rock" and user == "spock":
+	sys.stdout.write('the user won! spock vaporize rock\n')
+elif computer == "rock" and user == "paper":
+	sys.stdout.write('the user won! paper covers rock!\n')
+
 #lizard
 elif computer == "lizard" and user == "paper":
 	sys.stdout.write('the computer won! lizard eats paper!\n')
 elif computer == "lizard" and user == "spock":
 	sys.stdout.write('the computer won! lizard poisons spock!\n')
+
+elif computer == "lizard" and user == "scissors":
+	sys.stdout.write('the user won! scissor decapitates lizard!\n')
+elif computer == "lizard" and user == "rock":
+	sys.stdout.write('the user won! rock crushes lizard!\n')
+
 #spock
 elif computer == "spock" and user == "scissors":
 	sys.stdout.write('the computer won! spock smashes scissors!\n')
 elif computer == "spock" and user == "rock":
 	sys.stdout.write('the computer won! spock vaporizes rock!\n')
+
+elif computer == "spock" and user == "paper":
+	sys.stdout.write('the user won! paper disproves spock\n')
+elif computer == "spock" and user == "lizard":
+	sys.stdout.write('the user won! lizard poisons spock!\n')
+
 #scissors
 elif computer == "scissors" and user == "paper":
 	sys.stdout.write('the computer won! scissors cuts paper\n')
 elif computer == "scissors" and user == "lizard":
 	sys.stdout.write('the computer won! scissors decapitates lizard\n')
+
+elif computer == "scissors" and user == "spock":
+	sys.stdout.write('the user won! spock smashes scissors \n')
+elif computer == "scissors" and user == "rock":
+	sys.stdout.write('the user won! rock crushes scissors\n')
+
 #paper
 elif computer == "paper" and user == "spock":
 	sys.stdout.write('the computer won! paper disproves spock!\n')
 elif computer == "paper" and user == "rock":
 	sys.stdout.write('the computer won! paper covers rock!\n')
-elif computer == user:
-	sys.stdout.write('draw!\n')
+
+elif computer == "paper" and user == "lizard":
+	sys.stdout.write('the user won! lizard eats paper!\n')
+elif computer == "paper" and user == "scissors":
+	sys.stdout.write('the user won! scissors cuts paper!\n')
+
+#gelijkspel user en computer heben dezelfde keus
 else:
-	#ik kon ook dit gehele process andersom maken voor detailed situations maar dat zou zeker op een makkelijkere manier moeten kunnen di eik niet weet!
-	sys.stdout.write('user has won with '+user+' from '+computer+'!\n')
+	sys.stdout.write('draw!\n')
