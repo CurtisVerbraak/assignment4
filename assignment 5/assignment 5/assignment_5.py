322a027da93fd58f8c2a433316bbcde603fa7308
curtisverbraak <0896588@hr.nl>
2015-10-01 21:22:37 +0200

    done?

--- assignment 5/assignment 5/assignment_5.py
+++ assignment 5/assignment 5/assignment_5.py
@@ -6,6 +6,6 @@ text = raw_input("type something useless and press enter!\n")
 #https://docs.python.org/2/whatsnew/2.3.html
 #extended-slices <url over hoe je een reversed string KAN maken en andere opties
 # [begin:end:step]
-sys.stdout.write(text[13::-1]+'\n')
+sys.stdout.write(text[::-1]+'\n')
 ## end reverse string
 
